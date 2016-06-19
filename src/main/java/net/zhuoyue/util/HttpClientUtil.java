8c2990767bd7bbe6fc2ea1f4aa65c72be7e27384
zhuoyue <772766926@qq.com>
2016-06-19 21:01:43 +0800

    modify

---
+++ src/main/java/net/zhuoyue/util/HttpClientUtil.java
@@ -0,0 +1,15 @@
+package net.zhuoyue.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HttpClientUtil {
+	
+	private static Logger LOG = LogManager.getLogger(HttpClientUtil.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
