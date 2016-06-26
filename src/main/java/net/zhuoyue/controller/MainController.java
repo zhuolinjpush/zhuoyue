df25b2d69d8220ceaf90e073aab296394d233b1d
zhuolinjpush <772766926@qq.com>
2016-06-26 22:28:23 +0800

    modify zhuoyue

--- src/main/java/net/zhuoyue/controller/MainController.java
+++ src/main/java/net/zhuoyue/controller/MainController.java
@@ -1,15 +1,18 @@
 package net.zhuoyue.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/stat")
 public class MainController {
 	
-	@RequestMapping("/hello")
-	public String hello() {
-		return "hello world";
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public String hello(ModelMap model) {
+		model.addAttribute("message", "Spring 3 MVC Hello World");  
+        return "hello";
 	}
 
 }
--- src/main/webapp/WEB-INF/views/hello.jsp
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -1,14 +1,7 @@
 <%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
-
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-  <head>
-    <base href="<%=basePath%>">
-    
-    <title>My JSP 'MyJsp.jsp' starting page</title>
-  </head>
-  
   <body>
-    This is my JSP page. <br>
+    This is my JSP page. ${message} <br>
   </body>
 </html>
